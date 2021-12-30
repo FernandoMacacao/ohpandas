9f1146fbbec8dab7c1221c907c71aee2133eff55
FernandoMacacao <80410580+FernandoMacacao@users.noreply.github.com>
2021-12-30 13:17:45 +0000

    Update Timer.jsx

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -10,7 +10,7 @@ const Timer = () => {
 }
 
 const countdown = () => {
-  const countDate = new Date("January 7, 2022 00:00:00").getTime()
+  const countDate = new Date("February 1, 2022 00:00:00").getTime()
   const now = new Date().getTime()
   const gap = countDate - now
 
